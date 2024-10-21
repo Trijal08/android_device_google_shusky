d66e1bed7606b21e49b2f26c311e2bbbd087db5c
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-21 14:32:36 -0400

    Update device-husky.mk

--- device-husky.mk
+++ device-husky.mk
@@ -17,7 +17,7 @@
 # Restrict the visibility of Android.bp files to improve build analysis time
 $(call inherit-product-if-exists, vendor/google/products/sources_pixel.mk)
 
-RELEASE_KERNEL_HUSKY_DIR := device/google/shusky-kernels/24Q3-12065098
+RELEASE_KERNEL_HUSKY_DIR := device/google/shusky-kernels/5.15/24Q3-12065098
 
 TARGET_LINUX_KERNEL_VERSION := $(RELEASE_KERNEL_HUSKY_VERSION)
 # Keeps flexibility for kasan and ufs builds
