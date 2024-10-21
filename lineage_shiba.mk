e1d8d12e1be75e4a2edec0de4446186b823e2025
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-21 12:56:58 -0400

    Update lineage_shiba.mk

--- lineage_shiba.mk
+++ lineage_shiba.mk
@@ -4,9 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common stuff
+# Inherit some common StatiX stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 # Inherit device configuration
 DEVICE_CODENAME := shiba
@@ -19,7 +20,7 @@ $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := statix_$(DEVICE_CODENAME)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
